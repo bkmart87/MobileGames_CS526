9095f94a112f3a5e2a4bff37f267ff6342003884
Bethany <bkmart87@gmail.com>
2016-05-24 14:59:11 -0700

    First Commit
    Not much in this build. Just a title screen and an empty first level.

---
+++ Assets/Scripts/LoadNextLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadNextLevel : MonoBehaviour 
+{
+	public string levelName ="Level0"; 
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Application.LoadLevel(levelName); 
+		}
+	}
+}
