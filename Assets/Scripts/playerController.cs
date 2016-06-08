79b039e522a3f104d6ce54063a15ee65a23b01d8
davidpy <davidpy1992@gmail.com>
2016-06-08 12:22:38 -0700

    add david's scene

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	//movement variables
+	public float maxSpeed;
+
+
+	Rigidbody2D myRB;
+	Animator myAnim;
+
+	bool facingRight;
+
+	// Use this for initialization
+	void Start () {
+		myRB = GetComponent<Rigidbody2D> ();
+		myAnim = GetComponent<Animator> ();
+
+		facingRight = true;
+	
+	}
+
+
+	
+	// Update is called once per frame
+	void Update() {
+
+
+	}
+
+
+
+	void FixedUpdate () {
+		float move = Input.GetAxis ("Horizontal");
+		myAnim.SetFloat ("Speed", Mathf.Abs(move));
+
+		myRB.velocity = new Vector2 (move * maxSpeed, myRB.velocity.y);
+
+		if (move > 0 && !facingRight) {
+			flip ();
+		} else if (move < 0 && facingRight) {
+			flip ();
+		}
+
+	}
+
+	void flip() {
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
