08eb5ace1b91f001c72b46e50f205e3d021d5e8e
davidpy <davidpy1992@gmail.com>
2016-06-09 19:29:52 -0700

    test notes appearenceon on iPad
    Success.

---
+++ Assets/NoteTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoteTest : MonoBehaviour {
+	Rigidbody2D myRb;
+
+	// Use this for initialization
+	void Start () {
+		myRb = GetComponent<Rigidbody2D> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myRb.velocity = new Vector2 (-10, myRb.velocity.y);
+	}
+}
