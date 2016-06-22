588b1619c6ca45a091524e6998e310b5194e0569
davidpysp <davidpy1992@gmail.com>
2016-06-22 12:30:08 -0700

    rename two file

---
+++ Assets/Scripts/DavidScript/DestController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestController : MonoBehaviour {
+	GameObject game;
+
+	// Use this for initialization
+	void Start () {
+		game = GameObject.Find ("Game");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) { // player hit the destination and then "you win".
+		if (other.gameObject.layer == LayerMask.NameToLayer ("Player")) {
+			game.GetComponent<GameController>().GameWin();
+		}
+	}
+}
