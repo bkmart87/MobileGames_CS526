7bdf502c79331e5e3acc157924e5fca54860dd2b
davidpysp <davidpy1992@gmail.com>
2016-07-05 11:29:40 -0700

    add swing effect in mini map

--- Assets/Scripts/DavidScript/MiniMapController.cs
+++ Assets/Scripts/DavidScript/MiniMapController.cs
@@ -19,17 +19,37 @@ public class MiniMapController : MonoBehaviour {
 	public float scale = 0f;
 	public float distance = 0.2f;
 
+	float driftYPeter = 0.11f;
+	float driftYWolf = -0.12f;
+	float mapPeterCenter;
+	float mapWolfCenter;
+
 	// Use this for initialization
 	void Start () {
 		startX = mapStart.transform.localPosition.x;
 		length = mapDestination.transform.localPosition.x - mapStart.transform.localPosition.x;
+		mapPeterCenter = mapPeter.transform.localPosition.y;
+		mapWolfCenter = mapWolf.transform.localPosition.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		driftYPeter = Swing (mapPeter, mapPeterCenter, 2f, driftYPeter);
+		driftYWolf = Swing (mapWolf, mapWolfCenter, 2f, driftYWolf);
 		scale = notes.GetComponent<NoteMovement> ().progressScale;
 		mapPeter.transform.localPosition = new Vector3 (startX + length * scale, mapPeter.transform.localPosition.y, mapPeter.transform.localPosition.z);
 		distance = enemy.GetComponent<EnemyController> ().distance * 5f;
 		mapWolf.transform.localPosition = new Vector3 (mapPeter.transform.localPosition.x - distance, mapWolf.transform.localPosition.y, mapWolf.transform.localPosition.z);
+	}
+
+	float Swing (GameObject obj, float center, float y, float driftY) {
+		obj.transform.localPosition = new Vector3 (obj.transform.localPosition.x, obj.transform.localPosition.y + driftY, obj.transform.localPosition.z);
+		if (obj.transform.localPosition.y >= center + y || obj.transform.localPosition.y <= center - y) {
+			//Debug.Log ("drifty " + driftY);
+			driftY *= -1f;
+			obj.transform.localPosition = new Vector3 (obj.transform.localPosition.x, obj.transform.localPosition.y + driftY, obj.transform.localPosition.z);
+		} 
+		return driftY;
+		
 	}
 }
