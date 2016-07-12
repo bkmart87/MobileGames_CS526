8fcf5cc52229834b869e6ceb45ed02cc68a57d92
hx2011 <xhu075@usc.edu>
2016-07-12 11:39:41 -0700

    Update Game End Scene

---
+++ Assets/Scripts/GameEnd/GameEndHitText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class gameEndHitText : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		//gameObject.GetComponent<Text> ().text = Statistics.hit.ToString();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
